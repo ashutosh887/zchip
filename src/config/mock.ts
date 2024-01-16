baf6a06a036925f1142f4c7205ee59ecfe7e4f2b
Ashutosh Jha <ashutoshj887@gmail.com>
2024-01-16 17:01:12 +0530

    feat: add user configs

---
+++ src/config/mock.ts
@@ -0,0 +1,44 @@
+import { User } from "./types";
+
+export const sampleUsers: User[] = [
+  {
+    id: "1",
+    name: "Albert Einstein",
+    email: "einstein@example.com",
+    imageURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Einstein_1921_by_F_Schmutzer_-_restoration.jpg/440px-Einstein_1921_by_F_Schmutzer_-_restoration.jpg",
+  },
+  {
+    id: "2",
+    name: "CV Raman",
+    email: "cv.raman@example.com",
+    imageURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Sir_CV_Raman.JPG/440px-Sir_CV_Raman.JPG",
+  },
+  { id: "3", name: "Marie Curie", email: "marie.curie@example.com" },
+  {
+    id: "4",
+    name: "Isaac Newton",
+    email: "isaac.newton@example.com",
+    imageURL:
+      "https://upload.wikimedia.org/wikipedia/commons/f/f7/Portrait_of_Sir_Isaac_Newton%2C_1689_%28brightened%29.jpg",
+  },
+  { id: "5", name: "Charles Darwin", email: "charles.darwin@example.com" },
+  {
+    id: "6",
+    name: "Ada Lovelace",
+    email: "ada.lovelace@example.com",
+    imageURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ada_Lovelace_portrait.jpg/440px-Ada_Lovelace_portrait.jpg",
+  },
+  { id: "7", name: "Stephen Hawking", email: "stephen.hawking@example.com" },
+  {
+    id: "8",
+    name: "Nikola Tesla",
+    email: "nikola.tesla@example.com",
+    imageURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Tesla_circa_1890.jpeg/440px-Tesla_circa_1890.jpeg",
+  },
+  { id: "9", name: "Jane Goodall", email: "jane.goodall@example.com" },
+  { id: "10", name: "Galileo Galilei", email: "galileo.galilei@example.com" },
+];
